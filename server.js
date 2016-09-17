29aa76caddfa0f6b990e3b27a090c1d2c596eab8
arshpremi <arshpremi@gmail.com>
2016-09-18 00:47:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/article-one', function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+app.get('/articletwo', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
 
 app.get('/article-three', function (req, res) {
